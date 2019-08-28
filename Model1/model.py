404ef2bd1f1eee90889bcaf3f7843d5255924892
Leo Yeh <38709362+LeoYehSAS@users.noreply.github.com>
2019-08-28 18:22:55 +0800

    Update model.py

--- Model1/model.py
+++ Model1/model.py
@@ -32,15 +32,15 @@ comparison_metrics_df = pd.DataFrame(comparison_metrics_dict, columns=['accuracy
 
 
 import sasmm
-project = '影像辨識專案'
-model = '模型 1'
+project = 'F12-Metal1'
+model = 'Model1'
 sasmm.generateMetrics(project, model, confusion_metrics_df, 'confusion_metrics')
 sasmm.generateMetrics(project, model, comparison_metrics_df, 'comparison_metrics') 
 
 
 
 # model saving
-pickle.dump(model_01, open("/tsmc_model/model_files/模型 1.pkl", "wb"))
+pickle.dump(model_01, open("/tsmc_model/model_files/Model1.pkl", "wb"))
 
 #RT_METRICS:confusion_metrics_df
 #BT_METRICS:comparison_metrics_df
