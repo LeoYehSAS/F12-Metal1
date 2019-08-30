dfa4b0c10ac4b2dcd60f1cc740885a774ac05931
Leo Yeh <38709362+LeoYehSAS@users.noreply.github.com>
2019-08-30 17:30:32 +0800

    Update score.py

--- Model1/score.py
+++ Model1/score.py
@@ -1,65 +1,36 @@
 #INPUT:image_b64:string
 #OUTPUT:y_json:string
 #FUNCTION:score
-def score(image_b64):
-    import io, cv2, base64
-    import pandas as pd
-    import numpy as np
-    import random as rd
-    from PIL import Image
+from tensorflow.python.saved_model import tag_constants
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
-    # 01_read the image 
-#     x = cv2.imread('img001.jpg')
+MODEL_DIR = '/tsmc_model/model_files'
+version = 1
+import_path = os.path.join(MODEL_DIR, str(version))
 
-    # convert image to byte type
-#     with open('img001.jpg', "rb") as image_file:
-#         image_b64 = base64.b64encode(image_file.read())
-
-    # convert byte to string
-    image_b64_utf8 = image_b64.decode("utf-8")
-
-    # convert string to byte
-    image_b64_byte = str.encode(image_b64_utf8)
-
-    # convert byte to numpy array for cv2 
-    x = cv2.cvtColor(np.array(Image.open(io.BytesIO(base64.b64decode(image_b64_byte)))), cv2.COLOR_BGR2RGB)
-
-    x_resize = cv2.resize(x, (28, 28))
-    x_gray = cv2.cvtColor(x_resize, cv2.COLOR_BGR2GRAY)
-
-
-
-    # 02_data preparation
-    x_reshape = x_gray.reshape(1,784).astype('float32')
-    x_normalize = x_reshape / 255
-    
-    rd.seed(int(sum(x_normalize[0])))
-    tmp = rd.randrange(0,1000)/ 10000
-
-
-    # 03_data scoring
-    model = pickle.load(open("/tsmc_model/model_files/F12_Metal1_Model1.pkl", "rb"))
-
-    # predict the desired label for the incoming image.
-    y_pred = model.predict_classes(x_normalize)
-    y_pred = str(y_pred[0])
-
-    # calculate the responding probabilities of the labels for the specific image and show the 5 label with the highest probabilities.
-    y_prob = model.predict_proba(x_normalize)
-    y_prob = list(y_prob[0])
-
-    class_numeric = list(range(0, 10))
-    class_string = [str(x) for x in class_numeric]
-
-    y_prob_normalize = [(x - min(y_prob))/ (max(y_prob) - min(y_prob)) for x in y_prob]
-
-    y_prob_dict = {'label': class_string, 'probability': y_prob_normalize}
-    y_prob_df = pd.DataFrame.from_dict(y_prob_dict) 
-    y_prob_df.sort_values(by=['probability'], ascending=False, inplace=True)
-    y_prob_dict = pd.DataFrame.to_dict(y_prob_df[:5], orient='list')
-    y_prob_dict['probability'][0] -= tmp
-    
-    tmp2 = {'y_pred': y_pred, 'y_prob': y_prob_dict}
-    y_json = json.dumps(tmp2)
-    
-    return y_json
+graph = tf.Graph()
+with graph.as_default():
+    with tf.Session(graph=graph2) as sess:
+        meta_graph_def = tf.saved_model.loader.load(
+           sess,
+           [tag_constants.SERVING],
+           import_path
+        )
+        for op in sess.graph.get_operations():
+            print(op.name)
+        x = sess.graph.get_tensor_by_name('Conv1_input_5:0')
+        y = sess.graph.get_tensor_by_name('Softmax_5/Softmax:0')
+        y_out = sess.run(y, feed_dict = {x: [test_images[0]]})
+        y_json = np.argmax(y_out[0])
+        print(y_json)
